5d446b708ba397968482b2882245351c3221896e
xufu2017 <xufu2017@gmail.com>
2021-06-08 10:30:47 +0100

    f
    f

---
+++ e2e/images/.eslintrc.js
@@ -0,0 +1,38 @@
+﻿module.exports = {
+	root: true,
+	env: {
+	  node: true
+	},
+	'extends': [
+	  'plugin:vue/essential',
+	  'eslint:recommended',
+	  '@vue/typescript/recommended'
+	],
+	parserOptions: {
+		"parser":"@typescript-eslint/parser"
+	},
+	rules: {
+		"no-unused-vars":"warn",
+		"@typescript-eslint/no-inferrable-types": "off",
+		"@typescript-eslint/camelcase":["off"],
+		"@typescript-eslint/no-this-alias": [
+		  "error",
+		  {
+			"allowDestructuring": true, // Allow `const { props, state } = this`; false by default
+			"allowedNames": ["self"] // Allow `const self = this`; `[]` by default
+		  }
+		] 
+	},
+	overrides: [
+	  {
+		files: [
+		  '**/__tests__/*.{j,t}s?(x)',
+		  '**/tests/unit/**/*.spec.{j,t}s?(x)'
+		],
+		env: {
+		  jest: true
+		}
+	  }
+	]
+  }
+  
